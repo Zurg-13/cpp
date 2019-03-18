e17d0e8d4b2c68cd49aabac5a817727d472f00d9
BraunSS <BraunSS@tehnologia.com>
2019-03-18 19:45:48 +0300

    ++

--- Lens/src/main.cpp
+++ Lens/src/main.cpp
@@ -16,6 +16,8 @@ WLens * wgLens;
 // Главная функция. ------------------------------------------------------------
 //------------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
+
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
 
     WLog _wgLog; wgLog = &_wgLog;
